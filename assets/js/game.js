7cd61bf8519b35658ec05d4a04c0cd472e527e87
Torey Clark <clarktorey@gmail.com>
2023-05-14 19:12:53 -0500

    Paddle and keyboard controls

--- assets/js/game.js
+++ assets/js/game.js
@@ -22,11 +22,48 @@ function drawBall() {
     }
 }
 
+const paddleHeight = 10
+const paddleWidth = 75
+let paddleX = (canvas.width - paddleWidth) / 2
+let rightPressed = false
+let leftPressed = false
+function drawPaddle() {
+    if (rightPressed) {
+        paddleX = Math.min(paddleX + 7, canvas.width - paddleWidth)
+    } else if (leftPressed) {
+        paddleX = Math.max(paddleX - 7, 0)
+    }
+    ctx.beginPath()
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight)
+    ctx.fillStyle = '#0095dd'
+    ctx.fill()
+    ctx.closePath()
+}
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     drawBall()
+    drawPaddle()
     x += dx
     y += dy
 }
 
+function keyDownHandler(event) {
+    if (event.key === 'Right' || event.key === 'ArrowRight') {
+        rightPressed = true
+    } else if (event.key === 'Left' || event.key === 'ArrowLeft') {
+        leftPressed = true
+    }
+}
+
+function keyUpHandler(event) {
+    if (event.key === 'Right' || event.key === 'ArrowRight') {
+        rightPressed = false
+    } else if (event.key === 'Left' || event.key === 'ArrowLeft') {
+        leftPressed = false
+    }
+}
+
+document.addEventListener('keydown', keyDownHandler, false)
+document.addEventListener('keyup', keyUpHandler, false)
 setInterval(draw, 10)
